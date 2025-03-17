ba27b7ff23116ae23aff8545716311414a9f59f1
hyungkishin <tkaqkeldk@nate.com>
2025-03-17 19:35:27 +0900

    feat: comment table schema 설계

--- sql/schema.sql
+++ sql/schema.sql
@@ -19,3 +19,17 @@ CREATE TABLE IF NOT EXISTS `articles`
     `updated_at` DATETIME      NOT NULL COMMENT '최근 수정 시각'
     ) ENGINE = InnoDB
     DEFAULT CHARACTER SET = utf8mb4 COMMENT = '게시글';
+
+CREATE TABLE IF NOT EXISTS `comments`
+(
+
+    `comment_id`        BIGINT UNSIGNED NOT NULL PRIMARY KEY COMMENT '댓글 id',
+    `content`           VARCHAR(3000)   NOT NULL COMMENT '상세 내용',
+    `article_id`        BIGINT          NOT NULL COMMENT '게시글 id',
+    `parent_comment_id` BIGINT          NOT NULL COMMENT '상위 댓글 id',
+    `writer_id`         BIGINT          NOT NULL COMMENT '글쓴이 id',
+    `deleted`           TIMESTAMP       NOT NULL COMMENT '삭제 여부',
+    `created_at`        TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '생성 시각',
+    `updated_at`        TIMESTAMP       NULL COMMENT '최근 수정 시각'
+    ) ENGINE = InnoDB
+    DEFAULT CHARACTER SET = utf8mb4 COMMENT = '댓글';
