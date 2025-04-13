dbc788b3d0bceecd205bd010777a641e66e6aa9c
hyungkishin <tkaqkeldk@nate.com>
2025-04-13 22:50:51 +0900

    feat: 좋아요 domain gradle 설정

--- service/like/build.gradle.kts
+++ service/like/build.gradle.kts
@@ -1,4 +1,19 @@
 dependencies {
+    implementation(project(":common:snowflake"))
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("com.mysql:mysql-connector-j")
+    runtimeOnly("com.h2database:h2")
+
+    // 메인 클래스를 명시적으로 설정
+    tasks.getByName<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
+        mainClass.set("com.board.article.ArticleApplication")
+    }
+
 }
 
+allOpen {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Embeddable")
+}
